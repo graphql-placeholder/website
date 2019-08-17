32d3b0c99e8a5943e84bcd23c4895a8d3eb432e4
kingrayhan <rayhan095@gmail.com>
2019-08-18 05:12:00 +0600

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,13 @@
 module.exports = {
-    plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-sass`, `gatsby-plugin-styled-components`],
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-sass`,
+        `gatsby-plugin-styled-components`,
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: 'UA-145903220-1',
+            },
+        },
+    ],
 }
